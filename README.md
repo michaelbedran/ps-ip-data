1e2b02997e1a10407814ad05decad688327ebab8
michaelbedran <michaelbedran@users.noreply.github.com>
2022-09-10 21:45:58 -1000

    This is ready now
    Added Script running print out when the script starts to pull data.
    
    Changed ip-api url to include extra information
    
    Fixed weather api url due to erroneous error pertaining to variable types
    
    Now getting time from web api due to Windows being unable to accept ICU timezone format by default
    
    Added switch case for weather conditions
    
    Added switch case for wind direction
    
    Added hop and ping data to script
    
    Added the ability to output to JSON
    
    Added human friendly output of data
    
    Added checks to handle weather data, how hop is spelled depending on the amount of hops
    
    Added information to README

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # ps-ip-data
  Powershell Script to pull data pertaining to a particular IP address
+
+ To run, open a powershell prompt from the folder this script is located.
+
+ In PowerShell use the "Get-IP-Info" function to run the script
+
+ "Get-IP-Info" accepts the following commandline options:
+
+ -IP
+    The -IP tag does not need to be excplicitly stated. This will only allow a valid internet routable IP as an input. If no commandline option is given, the script will ask you for a valid IP.
+
+    Example:   ~/ps-ip-data> Get-IP-Info 1.1.1.1
+    Example 2: ~/ps-ip-data> Get-IP-Info -IP 1.1.1.1
+
+-j
+
+    The -j tag needs to be explicitly stated, but can be used without intially assigning a variable to -IP parameter. This tells the script to compile, and output the retreived data as a JSON object.
+
+    Example:   ~/ps-ip-data> Get-IP-Info 1.1.1.1 -j
+    Example 2: ~/ps-ip-data> Get-IP-Info -j
--- ps-ip-data.ps1
+++ ps-ip-data.ps1
@@ -1,8 +1,8 @@
-#function Get-IP-Info {
+function Get-IP-Info {
 
     Param(
     
-        [Parameter(Mandatory,ValueFromPipeline,Position=0)]
+        [Parameter(Mandatory,ValueFromPipeline)]
         <#
         [ValidatePattern] bracketed Regular expression tests for valid internet routable ipv4 address. 
         Tests for valid ipv4 address and filters out: 
@@ -20,83 +20,207 @@
         [Parameter()]
         [switch]$j
 
-       # [Parameter()]
-       # [string]$Path
     )
 
     Begin {
-       
-        <#
-        # Validate the $Path if set and $j is true. If not set, default $Path to User desktop if $j switch = true
-        if ($j) {
-            if ($Path) {
-                $PathValidated = $false
-                while (-not $PathValidated){
-                    if (Test-Path $Path) {
-                        if ((Get-Item $Path).PSisContainer) {
-                            $PathValidated = $true
-                        } else {
-                            Write-Host 'You must provide a path to a folder. The path cannot be a file.'
-                            $Path = Read-Host -Prompt 'Please input a valid path'
-                        }
-                        
-                    } else {
-                        Write-Host "Unable to locate: $Path"
-                        $Path = Read-Host -Prompt 'Please input a valid path'
-                    }
-                } 
-                
-            }
-        } else {
-            $Path = [Environment]::GetFolderPath("Desktop")
-        }
-        #>
+
+        Write-Host "Script is now running.."
 
         # Base url api for WHOIS through arin.net api
         $arin_api_url = 'http://whois.arin.net/rest'
+        
 
         # Header for Arin.net api
         $arin_header = @{"Accept"="application/xml"}
 
         # Base url for geolocation data through ip-api.com api
         $ip_api_url = 'http://ip-api.com/xml'
+        $ip_api_options = '?fields=status,message,country,countryCode,region,regionName,city,zip,lat,lon,timezone,offset,isp,org,as,asname,query'
 
         # Base url for weather data through open-meteo.com api
-        $open_meteo_api_url = "https://api.open-meteo.com/v1/forecast?latitude=$latitude&longitude=$longitude&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=auto"
+        $open_meteo_api_url = 'https://api.open-meteo.com/v1/forecast?latitude='
+        $open_meteo_api_url_2 = '&longitude='
+        $open_meteo_api_url_3 = '&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=auto'
+
+        # Base api for WorldTimeAPI
+        $world_time_api_url = "http://worldtimeapi.org/api/timezone/"
     }
 
     Process {
         
         # Get Whois Data
         $whois_url = "$arin_api_url/ip/$IP"
-        $whois_inf = Invoke-Restmethod $whois_url -Headers $arin_header
-
-        # For Testing... Remove in final#
-        Write-Host ($whois_info.net | Out-String)
-        # For Testing... Remove in final#
-
+        $whois_inf = Invoke-Restmethod $whois_url -Headers $arin_header     
+        
         #Get GeoLoc Data 
-        $geo_loc_url = "$ip_api_url/$IP"
+        $geo_loc_url = "$ip_api_url/$IP$ip_api_options"
         $geo_loc_inf = Invoke-RestMethod $geo_loc_url 
 
-        # For Testing... Remove in final#
-        Write-Host ($geo_loc_inf.query | Out-String)
-        # For Testing... Remove in final#
-
         # Read and set longitude and latitude
-        $latitude = ($geo_loc_inf.query.lat)
-        $longitude = ($geo_loc_inf.query.lon)
+        $lat = $geo_loc_inf.query.lat
+        $lon = $geo_loc_inf.query.lon
+        $latitude = [float]$lat
+        $longitude = [float]$lon
 
+        # Setting weather api to string due to error
+        $open_meteo_api_url = $open_meteo_api_url+$latitude+$open_meteo_api_url_2+$longitude+$open_meteo_api_url_3
+        
         # Get Weather data
         $weather_inf = Invoke-RestMethod $open_meteo_api_url
+        
+       
+        # Set timezone
+        $timezone = $geo_loc_inf.query.timezone
+        
+        # Get Ttime
+        $time_inf = Invoke-RestMethod "$world_time_api_url/$timezone"
 
-        # For Testing... Remove in final#
-        Write-Host ($weather_inf.current_weather | Out-String)
-        # For Testing... Remove in final#
+        # Store and calculate average latency of 10 pings to IP
+        $ping = Test-Connection $IP -Count 10 -ea 0
+        if ($ping) {
+            $average_latency = ($ping | Measure-Object -Property Latency -Average).Average
+        }
+        else {
+            $average_latency = "Ping to $IP failed" 
+        }
 
+        # Gathers and stores hop count
+        $number_of_hops = ((Test-Connection -Traceroute $IP) | Measure-Object -Property Hop -Maximum).Maximum
+        
 
-
-
-
+        # Set Weather condition 
+        switch ($weather_inf.current_weather.weathercode) {
+            {0 -contains $_} { $weather = "Clear Sky"}
+            {1, 2, 3 -contains $_} { $weather = "Mainly clear, partly cloudy, and overcast"}
+            {45, 48 -contains $_} { $weather = "Fog and depositing rime fog"}
+            {51, 53, 55 -contains $_} { $weather = "Drizzle: Light, moderate, and dense intensity"}
+            {56, 57 -contains $_} { $weather = "Freezing Drizzle: Light and dense intensity"}
+            {61, 63, 65 -contains $_} { $weather = "Rain: Slight, moderate and heavy intensity"}
+            {66, 67 -contains $_} { $weather = "Freezing Rain: Light and heavy intensity"}
+            {71, 73, 75 -contains $_} { $weather = "Snow fall: Slight, moderate, and heavy intensity"}
+            {77 -contains $_} { $weather = "Snow grains"}
+            {80, 81, 82 -contains $_} { $weather = "Rain showers: Slight, moderate, and violent"}
+            {85, 86 -contains $_} { $weather = "Snow showers slight and heavy"}
+            {95 -contains $_} { $weather = "Thunderstorm: Slight or moderate"}
+            {96, 99 -contains $_} { $weather = "Thunderstorm with slight and heavy hail"}    
+            default {$weather = "No Weather Data Available"}
+        }
+        
+        # Set Wind Direction
+        switch ($weather_inf.current_weather.winddirection) {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+            { ($_ -ge 0 ) -and ( $_ -lt 11.25)}  {$local_wind_direction = 'North'}
+            { ($_ -ge 11.25 ) -and ( $_ -lt 22.5)}  {$local_wind_direction = 'North by East'}
+            { ($_ -ge 22.5 ) -and ( $_ -lt 33.75)}  {$local_wind_direction = 'North-Northeast'}
+            { ($_ -ge 33.75 ) -and ( $_ -lt 45)}  {$local_wind_direction = 'Northeast by North'}
+            { ($_ -ge 45 ) -and ( $_ -lt 56.25)}  {$local_wind_direction = 'Northeast'}
+            { ($_ -ge 56.25 ) -and ( $_ -lt 67.5)}  {$local_wind_direction = 'Northeast by East'}
+            { ($_ -ge 67.5 ) -and ( $_ -lt 78.75)}  {$local_wind_direction = 'East-Northeast'}
+            { ($_ -ge 78.75 ) -and ( $_ -lt 90)}  {$local_wind_direction = 'East by North'}
+            { ($_ -ge 90 ) -and ( $_ -lt 101.25)}  {$local_wind_direction = 'East'}
+            { ($_ -ge 101.25 ) -and ( $_ -lt 112.5)}  {$local_wind_direction = 'East by South'}
+            { ($_ -ge 112.5 ) -and ( $_ -lt 123.75)}  {$local_wind_direction = 'East-Southeast'}
+            { ($_ -ge 123.75 ) -and ( $_ -lt 135)}  {$local_wind_direction = 'Southeast by East'}
+            { ($_ -ge 135 ) -and ( $_ -lt 146.25)}  {$local_wind_direction = 'Southeast'}
+            { ($_ -ge 146.25 ) -and ( $_ -lt 157.5)}  {$local_wind_direction = 'Southeast by South'}
+            { ($_ -ge 157.5 ) -and ( $_ -lt 168.75)}  {$local_wind_direction = 'South-Southeast'}
+            { ($_ -ge 168.75 ) -and ( $_ -lt 180)}  {$local_wind_direction = 'South by East'}
+            { ($_ -ge 180 ) -and ( $_ -lt 191.25)}  {$local_wind_direction = 'South'}
+            { ($_ -ge 191.25 ) -and ( $_ -lt 202.5)}  {$local_wind_direction = 'South by West'}
+            { ($_ -ge 202.5 ) -and ( $_ -lt 213.75)}  {$local_wind_direction = 'South-Southwest'}
+            { ($_ -ge 213.75 ) -and ( $_ -lt 225)}  {$local_wind_direction = 'Southwest by South'}
+            { ($_ -ge 225 ) -and ( $_ -lt 236.25)}  {$local_wind_direction = 'Southwest'}
+            { ($_ -ge 236.25 ) -and ( $_ -lt 247.5)}  {$local_wind_direction = 'Southwest by West'}
+            { ($_ -ge 247.5 ) -and ( $_ -lt 258.75)}  {$local_wind_direction = 'West-Southwest'}
+            { ($_ -ge 258.75 ) -and ( $_ -lt 270)}  {$local_wind_direction = 'West by South'}
+            { ($_ -ge 270 ) -and ( $_ -lt 281.25)}  {$local_wind_direction = 'West'}
+            { ($_ -ge 281.25 ) -and ( $_ -lt 292.5)}  {$local_wind_direction = 'West by North'}
+            { ($_ -ge 292.5 ) -and ( $_ -lt 303.75)}  {$local_wind_direction = 'West-Northwest'}
+            { ($_ -ge 303.75 ) -and ( $_ -lt 315)}  {$local_wind_direction = 'Northwest by West'}
+            { ($_ -ge 315 ) -and ( $_ -lt 326.25)}  {$local_wind_direction = 'Northwest'}
+            { ($_ -ge 326.25 ) -and ( $_ -lt 337.5)}  {$local_wind_direction = 'Northwest by North'}
+            { ($_ -ge 337.5 ) -and ( $_ -lt 348.75)}  {$local_wind_direction = 'North-Northwest'}
+            { ($_ -ge 348.75 ) -and ( $_ -le 360  )}  {$local_wind_direction = 'North by West'}            
+            
+            Default {"No Wind Data Available"}
+        }
     }
-#}
+
+    End {
+
+        # Setting variables
+        $owner_of_netblock = $whois_inf.net.orgRef.name
+        $average_latency = "$average_latency ms"        
+        $isp = $geo_loc_inf.query.isp
+        $as_number_owner = $geo_loc_inf.query.asname
+        $country = $geo_loc_inf.query.country
+        $region_name = $geo_loc_inf.query.regionName
+        $local_time = $time_inf.datetime
+        $local_temp = $weather_inf.current_weather.temperature
+        $local_windspeed = $weather_inf.current_weather.windspeed
+        $local_windspeed = "$local_windspeed mph"
+
+        # If it only takes one hop, hops in output is chnaged to hop. 
+        $h = "hops"
+        if ($number_of_hops -eq 1)
+        {
+            $h="hop"
+        }
+
+        #output data as json object if flag marked, otherwise output in friendly manor. 
+        if($j){
+            $json_temp = @{
+                'Ping Data' =@{
+                    'Number of Hops'= $number_of_hops
+                    'Average Ping Latency'= $average_latency
+                    }
+                
+                'Network Data' =@{
+                    'Netblock Owner'= $owner_of_netblock
+                    'ISP'= $isp
+                    'AS Number Owner'= $as_number_owner
+                    }
+
+                'Geo-Location Data' =@{
+                    'Country'= $country
+                    'Region'= $region_name
+                    'Latitude'= $latitude
+                    'Longitude'= $longitude
+                    'Timezone'= $timezone
+                    'Local Time'= $local_time
+                    }
+
+                'Weather Data' = @{
+                    'Temp'= $local_temp
+                    'Windspeed'= $local_windspeed
+                    'Wind Direction'= $local_wind_direction
+                    'Weather'= $weather
+                    }
+                }
+            
+                $json = $json_temp | ConvertTo-Json
+                Write-Host $json 
+        }
+        else {
+           Write-Host "It took $number_of_hops $h to get to this IP."
+           Write-Host "The average latency of 10 pings to the IP was $average_latency."
+           Write-Host "$owner_of_netblock owns the netblock."
+           Write-Host "$as_number_owner is the owner associated to the AS Number."
+           Write-Host "The ISP for this IP is $isp."
+           Write-Host "This IP is located in $country in the region of $region_name at the global coordinates of Latitude $latitude and Longitude $longitude."
+
+           if ($weather_inf){
+            Write-Host "The local weather shows as $weather, with a current windspeed of $local_windspeed. The winds are blowing due $local_wind_direction."
+            Write-Host "The current local date and time in the $timezone timezone is $local_time."
+            } else {
+            Write-Host "We were unable to retrieve the weather data. Please try again later."
+           }
+        }
+    }
+}
+
+
+
+
+    
+# ToDO Weather info switchs hops and latency 
